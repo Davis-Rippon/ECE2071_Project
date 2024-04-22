f809815704e6c0156285bdb68250a8b1561ceab5
Davis-Rippon <drip0002@student.monash.edu>
2024-04-22 18:52:26 +1000

    Added waitForUS to serial handler

--- src/CLI/serial_handler.py
+++ src/CLI/serial_handler.py
@@ -2,6 +2,6 @@ def start_recording(samplingRate: int, audioDuration: int, acceptableRange: int,
     print("started recording with: \n" +
         f"Sampling Rate: {samplingRate}\n" +
         f"Audio Duration: {audioDuration}\n" + 
-        f"Acceptable Range: {acceptableRange}\n"
-        f"Ultrasonic Sensor: {ultrasonicState} \n\n" + 
+        f"Ultrasonic Sensor: {'On' if ultrasonicState else 'Off'} \n" +
+        f"Wait for US to begin recording: {'Yes' if waitForUS else 'No'} \n\n" +
         "Your audio has been saved to outputs as a .wav (Test). \n")
